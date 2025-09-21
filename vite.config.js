f1f78eea32b8d4408e677ecda1f08af52836eda1
Mahak Bansal <mahakbansal82@gmail.com>
2025-09-21 16:26:56 +0530

    glob

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mahak-portfolio/',  // <--- THIS IS THE KEY
+  base:"/Mahak-portfolio",  // <--- THIS IS THE KEY
 });
